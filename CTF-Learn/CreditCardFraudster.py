bf94062a6476040a3743b7359ed816842aaf1335
Alexantoun <oz.antoun@gmail.com>
2022-04-30 00:41:42 -0600

    Completed Forensics101.sh

--- CTF-Learn/CreditCardFraudster.py
+++ CTF-Learn/CreditCardFraudster.py
@@ -5,5 +5,3 @@ import luhn
 for i in range (5432100000001234, 5432109999991234, 10000): #Increment by 10,000 upto 5432109....34
     if i%123457 == 0 and luhn.verify(str(i)):
         print(f'Value is: {i}')
-
-    
